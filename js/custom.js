5eb850fc26a11b09303e03d4615f3a05f189af11
VVulpes <66285048+B1ANKC-MOV@users.noreply.github.com>
2025-12-22 15:56:54 +0800

    new

--- js/custom.js
+++ js/custom.js
@@ -33,7 +33,7 @@ window.onload = function() {
 	  } else {
 		$('[rel="icon"]').attr('href', "/foz.ico");
 		$('[rel="shortcut icon"]').attr('href', "/foz.ico");
-		document.title = '(๑•̀ㅂ•́)و✧Hi~bro!';
+		document.title = '(๑•̀ㅂ•́)و✧Hi~welcome back!';
 		titleTime = setTimeout(function() {
 		  document.title = OriginTitile;
 		}, 2000);
